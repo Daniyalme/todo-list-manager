e17dd48f0a201f4d22a61a34b7d2a2e5ea7acf69
Daniyalme <mehraeen.daniyal@gmail.com>
2022-10-15 18:30:58 +0330

    Fixing the NewTask Modal

--- todo-list-manager/src/components/NewTaskModal.js
+++ todo-list-manager/src/components/NewTaskModal.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NewTaskModalActions } from "../store/NewTaskModalSlice";
+import dayjs from "dayjs";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Input from "@mui/material/Input";
+import Box from "@mui/material/Box";
+import Dialog from "@mui/material/Dialog";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import CloseIcon from "@mui/icons-material/Close";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { IconButton } from "@mui/material";
+import { css, cx } from "@emotion/css";
 
 const NewTaskModal = (props) => {
   const dispatch = useDispatch();
+  const ShowModal = useSelector((state) => state.newtaskmodal.ShowNewTaskModal);
 
   const InputNameHandler = (event) => {
     console.log(event.target.value);
@@ -13,32 +31,61 @@ const NewTaskModal = (props) => {
     console.log(event.target.value);
   };
 
-  const ClickCancelHandler = () => {
+  const InputDueDateHandler = (event) => {
+    console.log(event);
+  };
+
+  const HideModal = () => {
     dispatch(NewTaskModalActions.hide());
   };
 
-  // --------------------- addTask to DB should get implemented --------------------------
-  // const addTaskDB = (taskobj) => {
-  //   const db = getDatabase();
-  //   const Newid = props.maxid + 1;
+  const CloseButtonHandler = () => {
+    HideModal();
+  };
 
-  //   const reference = ref(db, "/tasks/" + taskobj.id);
-  //   const NewObj = {
-  //     id: Newid,
-  //     ...taskobj,
-  //   };
-  //   console.log("New Task :");
-  //   console.table(NewObj);
-  //   set(reference, NewObj);
-  //   tasksActions.addTask(NewObj);
-
-  //   console.log("New Task Added!");
-  // };
+  const ModalCloseHandler = () => {
+    HideModal();
+  };
 
   return (
-    <div className="modal">
-      <div className="modal-header">New Task Modal</div>
-      <div className="modal-body">
+    <Dialog open={ShowModal} onClose={ModalCloseHandler}>
+      <DialogTitle>Add Task</DialogTitle>
+      <IconButton
+        className={css`
+          position: absolute;
+          right: 24px;
+          top: 12px;
+        `}
+        onClick={CloseButtonHandler}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogContent>
+        <TextField
+          autoFocus
+          id="name"
+          label="Task Name"
+          type="email"
+          fullWidth
+          variant="outlined"
+          onChange={InputNameHandler}
+        />
+        {/* <Input
+          autoFocus
+          id="duedate"
+          label="Due To"
+          type={"datetime-local"}
+          variant="outlined"
+          onChange={InputDueDateHandler}
+        /> */}
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateTimePicker
+            label="Date&Time picker"
+            onChange={InputDueDateHandler}
+          />
+        </LocalizationProvider> */}
+      </DialogContent>
+      <Box>
         <form>
           <input
             type={"text"}
@@ -50,14 +97,13 @@ const NewTaskModal = (props) => {
             placeholder={"Description"}
             onChange={InputDescriptionHandler}
           />
-          <button type="button" onClick={ClickCancelHandler}>
+          <button type="button" onClick={CloseButtonHandler}>
             Cancel
           </button>
           <button type="submit">Add Task</button>
         </form>
-      </div>
-      <div className="modal-footer"></div>
-    </div>
+      </Box>
+    </Dialog>
   );
 };
 
